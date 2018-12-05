119ce7796b624b9dc2d4356c0bbc5b31647ae28e
Will Nixon <will_nixon@me.com>
2018-12-04 16:54:32 -0800

    tweaked style of wordtypes game

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -14,13 +14,13 @@ export function LandingPage(props) {
             </section>
             <section className="landing__wrapper">
                 <h2 className="landing__title">Learning Through Play</h2>
-                <h3 className="landing__subtitle">Reinforce your teaching through fun and interactive mini-games</h3>
+                <h3 className="landing__subtitle">English Rules reinforces lessons through fun and interactive mini-games that help solidify learning through gamification</h3>
                 <img className="landing__image" src={window.location.origin + "/landingpage1.png"} alt="Screenshot of Compound Sentences Game" />
                 <img className="landing__image" src={window.location.origin + "/landingpage2.png"} alt="Screenshot of Word Types Game" />
             </section>
             <section className="landing__wrapper">
                 <h2 className="landing__title">Customize Games and Build on the Basics</h2>
-                <h3 className="landing__subtitle">Adapt each game to fit your lesson plans and keep your students on their toes!</h3>
+                <h3 className="landing__subtitle">Each game can be adapted to fit what students are currently learning, ensuring unlimited variety and maintaining repeatability</h3>
             </section>
             <section className="landing__wrapper">
                 <h2 className="landing__title">Add New Games from the English Rules Library</h2>
