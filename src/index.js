459cd5b728a77146d683812b18b78ea133a7a937
Will Nixon <will_nixon@me.com>
2018-12-05 12:31:20 -0800

    fixed app src

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './store';
-import App from './components/App';
+import App from './components/@App/App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
